a8989e5f06368d8e65f4776ef1ac44c444ad42c4
Nicholas <32116122+nicholasmy@users.noreply.github.com>
2020-08-14 15:29:52 -0400

    Change the way the current permissions are sent to the sender for the allow command.

--- src/main/java/nicholasmy/darkrealms/CommandAllow.java
+++ src/main/java/nicholasmy/darkrealms/CommandAllow.java
@@ -12,6 +12,14 @@ public class CommandAllow implements CommandExecutor {
     public CommandAllow(DarkRealms darkRealms) {
     }
 
+    private void sendCurrentPermissions(CommandSender sender) {
+        // TODO list current permissions level, for now it's just made up values
+        sender.sendMessage(ChatColor.YELLOW + "Fly: " + ChatColor.RED + "NOBODY");
+        sender.sendMessage(ChatColor.YELLOW + "Set spawn: " + ChatColor.GOLD + "OP");
+        sender.sendMessage(ChatColor.YELLOW + "Go to spawn: " + ChatColor.GREEN + "ALL");
+        sender.sendMessage(ChatColor.RED + "To change permissions, use /allow <permission> <all|op|nobody>");
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         boolean allowedToRunCommand = false;
@@ -25,33 +33,25 @@ public class CommandAllow implements CommandExecutor {
                 allowedToRunCommand = true;
             }
         }
-        if (!allowedToRunCommand){
+        if (!allowedToRunCommand) {
             sender.sendMessage(ChatColor.RED + "You do not have permission to change permissions. Please talk to the owner of this Dark Realm if you believe this is an error.");
             return true;
         }
 
         // At this point, the sender is allowed to change permissions
 
-        if (args.length != 2){
+        if (args.length != 2) {
             sender.sendMessage(ChatColor.GREEN + "Here are the current permissions:");
-            // TODO list current permissions level, for now it's just made up values
-            sender.sendMessage(ChatColor.YELLOW + "Fly: " + ChatColor.RED +  "NOBODY");
-            sender.sendMessage(ChatColor.YELLOW + "Set spawn: " + ChatColor.GOLD +  "OP");
-            sender.sendMessage(ChatColor.YELLOW + "Go to spawn: " + ChatColor.GREEN +  "ALL");
-            sender.sendMessage(ChatColor.RED + "To change permissions, use /allow <permission> <all|op|nobody>");
         } else {
             // args length is 2
             String permission = args[0]; // Such as "fly"
             String level = args[1]; // such as "ops"
             // TODO actually change the permissions in the config file
             // TODO check if what the user entered is valid for both entries
-            sender.sendMessage(ChatColor.GREEN + " You successfully set the " + permission + " to " + level + "!");
+            sender.sendMessage(ChatColor.GREEN + "You successfully set the " + permission + " to " + level + "!");
+            sender.sendMessage(ChatColor.GREEN + "Here are the updated permissions:");
         }
-
-
-
-
-
+        sendCurrentPermissions(sender);
         return true;
     }
 }
