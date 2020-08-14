926c59dbc7d0a63736bd8ca4530a48b760eec5a9
Nicholas <32116122+nicholasmy@users.noreply.github.com>
2020-08-14 15:17:10 -0400

    Start basics of Allow command.

---
+++ src/main/java/nicholasmy/darkrealms/CommandAllow.java
@@ -0,0 +1,57 @@
+package nicholasmy.darkrealms;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandAllow implements CommandExecutor {
+
+    public CommandAllow(DarkRealms darkRealms) {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        boolean allowedToRunCommand = false;
+        if (sender instanceof ConsoleCommandSender) {
+            // If the console sends the command, allow it
+            allowedToRunCommand = true;
+        } else if (sender instanceof Player) {
+            // If a player who is op sends the command, allow it
+            Player p = (Player) sender;
+            if (p.isOp()) {
+                allowedToRunCommand = true;
+            }
+        }
+        if (!allowedToRunCommand){
+            sender.sendMessage(ChatColor.RED + "You do not have permission to change permissions. Please talk to the owner of this Dark Realm if you believe this is an error.");
+            return true;
+        }
+
+        // At this point, the sender is allowed to change permissions
+
+        if (args.length != 2){
+            sender.sendMessage(ChatColor.GREEN + "Here are the current permissions:");
+            // TODO list current permissions level, for now it's just made up values
+            sender.sendMessage(ChatColor.YELLOW + "Fly: " + ChatColor.RED +  "NOBODY");
+            sender.sendMessage(ChatColor.YELLOW + "Set spawn: " + ChatColor.GOLD +  "OP");
+            sender.sendMessage(ChatColor.YELLOW + "Go to spawn: " + ChatColor.GREEN +  "ALL");
+            sender.sendMessage(ChatColor.RED + "To change permissions, use /allow <permission> <all|op|nobody>");
+        } else {
+            // args length is 2
+            String permission = args[0]; // Such as "fly"
+            String level = args[1]; // such as "ops"
+            // TODO actually change the permissions in the config file
+            // TODO check if what the user entered is valid for both entries
+            sender.sendMessage(ChatColor.GREEN + " You successfully set the " + permission + " to " + level + "!");
+        }
+
+
+
+
+
+        return true;
+    }
+}
--- src/main/java/nicholasmy/darkrealms/DarkRealms.java
+++ src/main/java/nicholasmy/darkrealms/DarkRealms.java
@@ -8,6 +8,7 @@ public final class DarkRealms extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         this.saveDefaultConfig();
+        this.getCommand("allow").setExecutor(new CommandAllow(this));
 
     }
 
